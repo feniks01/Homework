aaf04d2352cc443041ee00bb070c776448af5895
feniks01 <108791050+feniks01@users.noreply.github.com>
2022-07-27 16:27:00 +0300

    Add files via upload

---
+++ bill.py
@@ -0,0 +1,36 @@
+class Bill:
+    def __init__(self,amount):
+        self.amount = amount
+        if amount < 0:
+            raise ValueError
+        if not isinstance(amount,int):
+            raise TypeError
+    
+    def __str__(self):
+        my_str = "Amount = " + str(self.amount) + " $ bill"
+        return my_str
+    
+    def __repr__(self) -> str:
+        my_str = "Amount = " + str(self.amount) + "$ bill"
+        return my_str
+    
+    def __int__ (self):
+        return int(self.amount)
+        
+    
+    def __eq__(self, bill) -> bool:
+        return self.amount == bill.amount
+    
+    def __hash__(self) -> int:
+        return hash(self.amount)
+
+       
+a = Bill(10)
+b = Bill(5)
+c = Bill(10)
+
+print (a)
+print (int(a))
+print (b)
+
+
---
+++ from_exrsise01_import_Bill.py
@@ -0,0 +1,6 @@
+from exrsise01 import Bill
+class Batchbill:
+    def __init__(self, list_of_bills):
+        self.list_of_bills  = list_of_bills
+    def __str__(self) -> str:
+        my_str = 
