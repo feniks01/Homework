eab897a0658f0f7651143baaac79d18ef03a7325
feniks01 <108791050+feniks01@users.noreply.github.com>
2022-07-27 16:26:37 +0300

    Add files via upload

---
+++ num__set.py
@@ -0,0 +1,16 @@
+numbers = set()
+num = [4, 5, 66]
+n = {33, 0, 88}
+newnum = []
+numbers.update(n, num, )
+ll =[x for x in range(51)]
+numbers.update(ll)
+
+print (numbers)
+
+for x in numbers:
+    if "a" in numbers:
+        newnum.append(x)
+
+print(newnum)
+
